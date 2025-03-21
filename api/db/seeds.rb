0b94eee31a336f3a3e125209206ee41fdd8b080f
Yassine Allaouna <yassineallaouna@MacBook-Pro-de-Yass.local>
2025-03-21 21:49:54 +0100

    Ajout du frontend live + auth + navbar

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+user = User.first || User.create(email: "admin@mail.com", password: "123456")
+
+10.times do
+  user.employees.create(
+    name: Faker::Name.name,
+    position: %w[Développeur Designer RH Marketing].sample,
+    department: %w[Tech Produit RH Finance].sample
+  )
+end
